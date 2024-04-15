d467ace5899e1570004aba41308f3fe85ea63603
abraham1229 <abrahamortizcastro1229@gmail.com>
2024-04-15 12:26:35 -0600

    SE agregan launch y params

---
+++ reto2/launch/params_launch.py
@@ -0,0 +1,40 @@
+#Importaciones necesarias
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from launch import LaunchDescription
+
+def generate_launch_description():
+
+    #Se declara la dirección del launchfile
+    config = os.path.join(
+        get_package_share_directory('reto2'),
+        'config',
+        'params.yaml'
+        )
+    
+    #Se corre el signal generator
+    talker_node = Node(
+        package='reto2',
+        executable='path_generator',
+        output='screen',
+        parameters = [config]
+    )
+
+    #Se ejecuta el graph
+    rqt_graph_node = Node(
+        package='rqt_graph',
+        executable='rqt_graph',
+        output='screen',
+    )
+
+    #Se ejecuta el plot
+    rqt_plot_node = Node(
+        package='rqt_plot',
+        executable='rqt_plot',
+        output='screen',
+        arguments=[],  # Se especifican los tópicos a graficar
+    )
+
+    l_d = LaunchDescription([talker_node, rqt_graph_node, rqt_plot_node])
+    return l_d
