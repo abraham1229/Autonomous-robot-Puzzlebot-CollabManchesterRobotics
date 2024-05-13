75c2be7346d8c4afad2945f2f87c07e4c49e4fd5
abraham1229 <abrahamortizcastro1229@gmail.com>
2024-05-13 15:21:02 -0600

    Se agrega la copia del reto5

---
+++ reto5/launch/seguidor_launch.py
@@ -0,0 +1,59 @@
+#Importaciones necesarias
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from launch import LaunchDescription
+
+def generate_launch_description():
+
+    #Se declara la dirección del launchfile
+    config = os.path.join(
+        get_package_share_directory('reto4'),
+        'config',
+        'params.yaml'
+        )
+    
+
+    odometry_node = Node(
+        package='reto4',
+        executable='odometry',
+        output='screen'
+    )
+
+    path_generator_node = Node(
+        package='reto4',
+        executable='path_generator',
+        output='screen'
+    )
+
+    controller_node = Node(
+        package='reto4',
+        executable='controller',
+        output='screen'
+    )
+
+    color_detection_node = Node(
+        package='reto4',
+        executable='color_detection',
+        output='screen',
+        parameters = [config]
+    )
+
+    #Se ejecuta el graph
+    rqt_graph_node = Node(
+        package='rqt_graph',
+        executable='rqt_graph',
+        output='screen',
+    )
+
+    #Se ejecuta el plot
+    rqt_plot_node = Node(
+        package='rqt_plot',
+        executable='rqt_plot',
+        output='screen',
+        arguments=[],  # Se especifican los tópicos a graficar
+    )
+    
+    
+    l_d = LaunchDescription([odometry_node,path_generator_node,controller_node,color_detection_node])
+    return l_d
--- reto5/setup.py
+++ reto5/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'reto5'
@@ -10,6 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share',package_name,'launch'),
+         glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +25,10 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'odometry = reto5.odometry:main'
+            'odometry = reto5.odometry:main',
+            'controller = reto4.controller:main',
+            'path_generator = reto4.path_generator:main',
+            'color_detection = reto4.color_detection:main',
         ],
     },
 )
