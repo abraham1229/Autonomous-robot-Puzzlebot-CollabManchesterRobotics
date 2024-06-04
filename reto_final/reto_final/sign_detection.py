16b2a721f00e77e545b335ec7d9ec315978882a7
abraham1229 <abrahamortizcastro1229@gmail.com>
2024-06-04 11:14:32 -0600

    Se agrega deteccion del más cercano solamente

--- reto_final/reto_final/sign_detection.py
+++ reto_final/reto_final/sign_detection.py
@@ -10,7 +10,6 @@ from msgs_clase.msg import Signal   # type: ignore
 from yolov8_msgs.msg import InferenceResult # type: ignore
 from yolov8_msgs.msg import Yolov8Inference # type: ignore
 
-
 bridge = CvBridge()
 
 class Camera_subscriber(Node):
@@ -18,7 +17,7 @@ class Camera_subscriber(Node):
     def __init__(self):
         super().__init__('camera_subscriber')
 
-        self.model = YOLO('/home/abraham/DeteccionSeniales2.pt')
+        self.model = YOLO('/home/abraham/modelos/DeteccionSeniales3_640.pt')
 
         self.yolov8_inference = Yolov8Inference()
 
@@ -38,20 +37,15 @@ class Camera_subscriber(Node):
         self.timer = self.create_timer(self.timer_period, self.timer_callback_signs)
 
         self.img = np.ones((480, 640, 3), dtype=np.uint8)
-        # Mensaje de que el nodo ha sido inicializado
         self.get_logger().info('7')
 
     def camera_callback(self, data):
         if data is not None:
             self.img = bridge.imgmsg_to_cv2(data, "bgr8")
-            # Voltear la imagen horizontalmente y luego verticalmente
             self.img = cv2.rotate(self.img, cv2.ROTATE_180)
 
-        
-        
     def timer_callback_signs(self):
-
-        results = self.model(source=self.img,conf=0.4, verbose=True)
+        results = self.model(source=self.img, conf=0.4, verbose=False)
         self.yolov8_inference.header.frame_id = "inference"
         self.yolov8_inference.header.stamp = self.get_clock().now().to_msg()
 
@@ -59,71 +53,74 @@ class Camera_subscriber(Node):
             boxes = r.boxes
             for box in boxes:
                 self.inference_result = InferenceResult()
-                b = box.xyxy[0].to('cpu').detach().numpy().copy()  # get box coordinates in (top, left, bottom, right) format
+                b = box.xyxy[0].to('cpu').detach().numpy().copy()
                 c = box.cls
                 self.inference_result.class_name = self.model.names[int(c)]
-                self.inference_result.top = int(b[0])
-                self.inference_result.left = int(b[1])
-                self.inference_result.bottom = int(b[2])
-                self.inference_result.right = int(b[3])
+                self.inference_result.top = int(b[1])
+                self.inference_result.left = int(b[0])
+                self.inference_result.bottom = int(b[3])
+                self.inference_result.right = int(b[2])
                 self.yolov8_inference.yolov8_inference.append(self.inference_result)
-
-            #self.get_logger().info(f"{self.yolov8_inference}")
-
-        self.escribirMensaje(self.yolov8_inference)
 
         annotated_frame = results[0].plot()
         img_msg = bridge.cv2_to_imgmsg(annotated_frame, encoding="bgr8")
-  
 
         self.img_pub.publish(img_msg)
         self.yolov8_pub.publish(self.yolov8_inference)
-        self.predi_pub.publish(self.senialesDetectadas)
-        
+
+        self.escribirMensaje(self.yolov8_inference)
+
         self.yolov8_inference.yolov8_inference.clear()
         self.valoresObtenidos.clear()
 
-    
-    def escribirMensaje(self,yoloInference):
-        # Manda las clases que obtiene
+    def escribirMensaje(self, yoloInference):
         self.senialesDetectadas = Signal()
-        
-        for inference in yoloInference.yolov8_inference:
-                self.valoresObtenidos.append(inference.class_name)
+        max_area = 0
+        signal_with_max_area = None
 
-        for class_name in self.valoresObtenidos:
+        for inference in yoloInference.yolov8_inference:
+            class_name = inference.class_name
+            area = self.calcularArea(inference.top, inference.left, inference.bottom, inference.right)
+
+            if class_name == "dotLine":
+                if area > self.get_line_threshold():
+                    self.senialesDetectadas.dot_line = True
+            else:
+                if area > max_area:
+                    max_area = area
+                    signal_with_max_area = inference
+
+        if signal_with_max_area:
+            class_name = signal_with_max_area.class_name
+            print(class_name)
             if class_name == "aheadOnly": 
                 self.senialesDetectadas.ahead_only = True
-            
-            elif class_name == "dotLine": 
-                self.senialesDetectadas.dot_line = True
-            
             elif class_name == "giveWay": 
                 self.senialesDetectadas.give_way = True
-            
             elif class_name == "greenLight": 
                 self.senialesDetectadas.green_light = True
-            
             elif class_name == "redLight": 
                 self.senialesDetectadas.red_light = True
-            
             elif class_name == "roadwork": 
                 self.senialesDetectadas.roadwork = True
-            
             elif class_name == "roundabout": 
                 self.senialesDetectadas.roundabout = True
-            
             elif class_name == "stop": 
                 self.senialesDetectadas.stop = True
-            
             elif class_name == "turnLeft": 
                 self.senialesDetectadas.turn_left = True
-            
             elif class_name == "turnRight": 
                 self.senialesDetectadas.turn_right = True
-            
             elif class_name == "yellowLight": 
                 self.senialesDetectadas.yellow_light = True
+
+        self.predi_pub.publish(self.senialesDetectadas)
+
+    def calcularArea(self, top, left, bottom, right):
+        ancho = abs(right - left)
+        alto = abs(bottom - top)
+        area = ancho * alto
+        return area
 
 
 def main(args=None):
