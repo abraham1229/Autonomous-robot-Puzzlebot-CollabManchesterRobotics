cd02f9986f52e2ca0c628c45a8c154c60a97b621
abraham1229 <abrahamortizcastro1229@gmail.com>
2024-06-08 14:07:52 -0600

    se agrega condición de roundabout

--- reto_final/reto_final/sign_detection.py
+++ reto_final/reto_final/sign_detection.py
@@ -5,7 +5,7 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
-from msgs_clase.msg import Signal   # type: ignore
+from msgs_clase.msg import Signal,Dotline   # type: ignore
 
 from yolov8_msgs.msg import InferenceResult # type: ignore
 from yolov8_msgs.msg import Yolov8Inference # type: ignore
@@ -22,11 +22,18 @@ class Camera_subscriber(Node):
         self.model = YOLO('/home/abraham/modelos/dotline.pt')
 
         self.yolov8_inference = Yolov8Inference()
-        # Realiza la suscripción d
+        # Realiza la suscripción de la imágen
         self.subscription = self.create_subscription(
             Image,
             '/sign_information',
             self.camera_callback,
+            10) 
+        
+        # Realiza la suscripción a la línea vertical
+        self.subscription = self.create_subscription(
+            Dotline,
+            '/vertical_bool',
+            self.vertical_callback,
             10) 
 
         self.yolov8_pub = self.create_publisher(Yolov8Inference, "/Yolov8_Inference", 1)
@@ -48,12 +55,24 @@ class Camera_subscriber(Node):
         self.dot_line_detected_time = None
         self.dot_line_sent = False
 
+        # Se pone el tipo de mensaje que va leer el tipo de vertical
+        self.lineaRight = False
+        self.lineaLeft = False
+        self.lineaBoth = False
+
         
         self.get_logger().info('Sign detection node initialized')
 
     def camera_callback(self, data):
         if data is not None:
             self.img = bridge.imgmsg_to_cv2(data, "bgr8")
+    
+    def vertical_callback(self, data):
+        if data is not None:
+            self.lineaBoth = data.both
+            self.lineaRight = data.right
+            self.lineaLeft = data.left
+    
 
     def timer_callback_signs(self):
         results = self.model(source=self.img, conf=0.4, verbose=False)
@@ -85,19 +104,23 @@ class Camera_subscriber(Node):
         self.valoresObtenidos.clear()
 
     def escribirMensaje(self, yoloInference):
+        # Se crea el mensaje a publicar.
         self.senialesDetectadas = Signal()
+        # Variables para calcular la senial más cercana.
         max_area = 0
         signal_with_max_area = None
 
-    
+        # Por cada inferencia que se tuvo se toman decisiones dependiendo
+        # de lo que detecto.
         for inference in yoloInference.yolov8_inference:
             class_name = inference.class_name
             nearest = inference.bottom
             
-
+            # Si es dotLine se manda el mensaje para saber que se encuentra en 
+            # un cruce.
             if class_name == "dotLine":
-                print(nearest)
-                if nearest > 0: # 220
+                #print(nearest)
+                if nearest > 260:
                     if not self.dot_line_detected_time:
 
                         self.dot_line_detected_time = time.time()
@@ -114,15 +137,19 @@ class Camera_subscriber(Node):
                             self.dot_line_sent = False
                             self.dot_line_detected_time = None
 
+
+            # Si la inferencia es diferente a un tipo de línea punteda se calcula
+            # cual es el más cercano
             if class_name != "dotLine":
                 if nearest > max_area:
                         max_area = nearest
                         signal_with_max_area = inference
 
 
+        # En el caso de que se haya encontrado senial se mandan como true solamente
+        # si están a cierta distancia.
         if signal_with_max_area:
             #self.get_logger().info(f'{signal_with_max_area.bottom})')
-            
             class_name = signal_with_max_area.class_name
             if class_name == "aheadOnly": 
                 self.senialesDetectadas.ahead_only = True
@@ -133,25 +160,27 @@ class Camera_subscriber(Node):
                         self.senialesDetectadas.give_way = True
                         self.senialesDetectadas.dot_line = False
 
-            elif class_name == "greenLight": 
-                self.senialesDetectadas.green_light = True
+            elif class_name == "greenLight":
+                if signal_with_max_area.bottom > 115: 
+                    self.senialesDetectadas.green_light = True
             elif class_name == "redLight": 
-                self.senialesDetectadas.red_light = True
+                if signal_with_max_area.bottom > 120:
+                    self.senialesDetectadas.red_light = True
 
             elif class_name == "roadwork": 
-                if signal_with_max_area.bottom > 80:
+                if signal_with_max_area.bottom > 110:
                     self.senialesDetectadas.roadwork = True
 
             elif class_name == "roundabout":
-                if signal_with_max_area.bottom > 70:
-                    if self.dot_line_detected_time:
-                        elapsed_time = time.time() - self.dot_line_detected_time
-                        if elapsed_time > 2.0:
-                            self.senialesDetectadas.roundabout = False
-                            self.senialesDetectadas.dot_line = False
+                #if signal_with_max_area.bottom > 70:
+                if self.dot_line_detected_time:
+                    elapsed_time = time.time() - self.dot_line_detected_time
+                    if elapsed_time > 2.0:
+                        self.senialesDetectadas.roundabout = True
+                        self.senialesDetectadas.dot_line = False
 
             elif class_name == "stop":
-                if signal_with_max_area.bottom > 80:
+                if signal_with_max_area.bottom > 110:
                     # Si detectamos "stop", verificamos el tiempo
                     if not self.stop_signal_sent:
                         self.stop_detected_time = time.time()
@@ -160,10 +189,10 @@ class Camera_subscriber(Node):
                     else:
                         elapsed_time = time.time() - self.stop_detected_time
             
-                        if elapsed_time < 2.0:
+                        if elapsed_time < 5.0:
                             self.senialesDetectadas.stop = True
 
-                        elif elapsed_time >= 8.0:
+                        elif elapsed_time >= 10.0:
                             self.stop_signal_sent = False
                             self.stop_detected_time = None
 
@@ -172,9 +201,23 @@ class Camera_subscriber(Node):
             elif class_name == "turnRight": 
                 self.senialesDetectadas.turn_left = True
             elif class_name == "yellowLight":
-                self.senialesDetectadas.yellow_light = True
+                if signal_with_max_area.bottom > 120:
+                    self.senialesDetectadas.yellow_light = True
+            
+            # Se hace la condición de dirección
+            if self.senialesDetectadas.roundabout:
+                    
+                if self.lineaLeft:
+                    self.senialesDetectadas.turn_left = True
+                elif self.lineaRight:
+                    self.senialesDetectadas.turn_right = True
+                elif self.lineaBoth:
+                    self.senialesDetectadas.turn_right = True
+
         
+    
         self.predi_pub.publish(self.senialesDetectadas)
+
 
 
 def main(args=None):
